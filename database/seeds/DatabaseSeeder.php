d3404c6cd47afb211007db32ab486fe30cc098e4
GlennKimbleJr <glennkimble@yahoo.com>
2019-03-29 00:27:22 -0400

    Added user table seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(BoatsTableSeeder::class);
         $this->call(CustomerTableSeeder::class);
+        $this->call(UserTableSeeder::class);
     }
 }
